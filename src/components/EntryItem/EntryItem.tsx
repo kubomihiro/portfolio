1be90c699fee1ccae333e403b55d4255264a91e6
kubomihiro <kubomihimihiron@gmail.com>
2023-12-06 23:27:34 +0900

    色変更

--- src/components/EntryItem/EntryItem.tsx
+++ src/components/EntryItem/EntryItem.tsx
@@ -20,6 +20,9 @@ import styles from "./EntryItem.module.css";
     <article className={styles.entryItem}>
       <h2 className={styles.entryItemTitle}>
         <Link href={`/entry/${props.id}`}>{props.title}</Link>
+
+
+        
       </h2>
       {props.description && (
         <p className={styles.description}>{props.description}</p>
