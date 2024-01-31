635fc3818b35656e3d167b9e7d0ded9c7cc75325
kubomihiro <kubomihimihiron@gmail.com>
2024-01-31 20:21:11 +0900

    Header変更

--- src/components/Heater/Header.tsx
+++ src/components/Heater/Header.tsx
@@ -7,7 +7,7 @@ export const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>
-        <Link href='/'>Portfolio</Link>
+        <Link href='/'>Mihiro's Portfolio</Link>
       </h1>
     </header>
   )
