2d0d1cfa47ad27ed4b8ec083dc594bd8cdc29c4a
kubomihiro <kubomihimihiron@gmail.com>
2024-02-19 23:24:45 +0900

    キャッシュ戻してみた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
       offset: 0,
       limit: 4,
       //↑取得する件数
-      filters: 'publishedAt[greater_than]2024-02-19',
+      filters: 'publishedAt[greater_than]2023-11-10',
       //上の数字直すとキャッシュクリアされる
       fields: 'id,title,category,publishedAt,eyecatch,description',
     },
