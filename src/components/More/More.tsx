15f90c21e8bf3fbbe71f1ad2151922c14d2e9ecf
kubomihiro <kubomihimihiron@gmail.com>
2024-02-20 13:32:52 +0900

    more　変更

--- src/components/More/More.tsx
+++ src/components/More/More.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export const More = () => {
   return (
     <p className={styles.more}>
-      <Link href='/works'>and More</Link>
+      <Link href='/works'>and more</Link>
     </p>
   )
 }
