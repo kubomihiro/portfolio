ac515f1ad4dcb77ce99da86912f84fbb71708207
kubomihiro <kubomihimihiron@gmail.com>
2024-02-21 23:32:28 +0900

    css変更

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -16,7 +16,7 @@ export const About = () => {
           </figure>
           <section className={styles.textContainer}>
             <h3 className={styles.aboutName}>久保　美尋 Kubo Mihiro</h3>
-            <p>2003年 3月 埼玉県出身</p>
+            <p  className={styles.text}>2003年 3月 埼玉県出身<br />
             {/* <p>
             2021年 3月 浦和実業学園高等学校 普通科課程 卒業
             <br />
@@ -26,8 +26,6 @@ export const About = () => {
             <br />
             2025年 3月 産業能率大学 情報マネジメント学部 現代マネジメント学科 通信教育課程 卒業見込
           </p> */}
-          
-            <p>
               高校卒業後、通信制大学で学びながら、2年間音響に打ち込みました。<br />
               音楽に関連するウェブサイト、ロゴ、フライヤーなどからインスピレーションを受け、デザインの魅力に惹かれました。<br />
               音楽への愛着から、高校時代からライブハウスに足繁く通い、今でも月に最大3回は足を運んでいます。
